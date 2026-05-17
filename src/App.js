03dea7c0bbc92e6a2e576dfd6a4573f6d0103e19
Vrushabh <vrushabgorivale42740@gmail.com>
2026-05-17 17:41:36 +0530

    fix: move index.html to public folder

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import AuthForm from './AuthForm';
+
+function App() {
+  return (
+    <div><AuthForm/></div>
+  );
+}
+
+export default App;
---
+++ src/AuthForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+export default function Authform(){
+    const [islogin, setIsLogin] = useState(true);
+    return(
+        <div className='container'>
+            <div className='form-container'>
+                <div className='form-toggle'>
+                    <button className={islogin ? 'active':""} onClick={() => setIsLogin(true)}>Login</button>
+                    <button className={!islogin ? 'active':""} onClick={() => setIsLogin(false)}>SignUp</button>
+                </div>
+                {islogin ? <>
+                <div className='form'>
+                    <h2>Login Form</h2>
+                    <input type='email' placeholder='Email'/>
+                    <input type='password' placeholder='Password'/>
+                    <a href='#'>Forgot Password?</a>
+                    <button>Login</button>
+                    <p>Not a Member? <a href='#' onClick={() => setIsLogin(false)}>SignUp Now</a></p>
+                </div>
+                </> : <>
+                <div className='form'>
+                    <h2>SignUp Form</h2>
+                    <input type='email' placeholder='Email'/>
+                    <input type='password' placeholder='Password'/>
+                    <input type='password' placeholder='Confirm password'/>
+                    <button>SignUp</button>
+                    <p>Already a Member? <a href='#' onClick={() => setIsLogin(true)}>Login Now</a></p>
+                </div>
+                </>}
+            </div>
+        </div>
+    )
+}
